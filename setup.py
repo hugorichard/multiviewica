5d30893982076637795c3a44754580d12b3907dc
Pierre Ablin <pierreablin@gmail.com>
2020-06-09 19:12:44 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="multiviewica",
     install_requires=["scikit-learn"],
-    author="Hugo RICHARD, Luigi GRESELE",
-    author_email="hugo.richard@inria.fr, luigi.gresele@inria.fr",
-    description="Non linear SRM",
+    author="Hugo RICHARD, Luigi GRESELE, Pierre ABLIN",
+    author_email="hugo.richard@inria.fr, luigi.gresele@inria.fr, pierre.ablin@gmail.com",
+    description="Multi-view ICA",
     keywords="",
     packages=find_packages(),
     python_requires=">=3",
