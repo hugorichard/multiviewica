84dccc8d2ef9fda3657aa7418bf449b6d0bbd4eb
Pierre Ablin <pierreablin@gmail.com>
2020-06-10 15:06:10 +0200

    readme

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="multiviewica",
-    install_requires=["scikit-learn"],
     author="Hugo RICHARD, Luigi GRESELE, Pierre ABLIN",
     author_email="hugo.richard@inria.fr, luigi.gresele@inria.fr, pierre.ablin@gmail.com",
     description="Multi-view ICA",
     keywords="",
     packages=find_packages(),
     python_requires=">=3",
+    install_requires=['numpy>=1.12', 'scipy>=0.18.0',
+                      'matplotlib>=2.0.0',
+                      'scikit-learn>=0.23', 'mne>=0.20', 'python-picard',
+                      'nibabel', 'fastsrm', 'nilearn']
 )
