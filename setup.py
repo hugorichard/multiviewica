4e6d999306df06133f69044f6a27c63f6d4e946c
hrichard <hugo.richard@inria.fr>
2020-06-10 16:02:34 +0200

    anonymize

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="multiviewica",
-    author="Hugo RICHARD, Luigi GRESELE, Pierre ABLIN",
-    author_email="hugo.richard@inria.fr, luigi.gresele@inria.fr, pierre.ablin@gmail.com",
     description="Multi-view ICA",
     keywords="",
     packages=find_packages(),
